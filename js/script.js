b5bf293abb87d91150d8e4d45795301b93b440b8
Freedteck <freedteck@gmail.com>
2023-10-11 02:57:59 +0100

    build game with more functionality

--- js/script.js
+++ js/script.js
@@ -1,80 +1,131 @@
+// Variables
+const boardElements = document.querySelectorAll('.board');
+const main = document.querySelector('main');
+const dialog = document.querySelector('.dialog');
+const startBtn = document.getElementById('start');
+const sideBar = document.querySelector('.sidebar');
+const resetBtn = document.getElementById('reset');
+const popUp = document.getElementById('pop-up')
+const continueBtn = document.getElementById('continue')
+const span = document.querySelector('span')
+const closeBtn = document.querySelector('.close')
+
+const player1Score = document.getElementById('score1');
+const player2Score = document.getElementById('score2');
+
+// Game Board Module
 const gameBoard = (function () {
-    const gameBoardChild = ['', '', '', '', '', '', '', '', '']
-    const value = ['X', 'O']
+    const board = ['', '', '', '', '', '', '', '', ''];
+    const players = ['X', 'O'];
 
     const winningCombos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [2, 4, 6], [0, 4, 8]
-    ]
+    ];
 
-    const winning = () => {
-        for (let combo of winningCombos) {
-            const [a, b, c] = combo
-            if (gameBoardChild[a] && gameBoardChild[a] === gameBoardChild[b]
-                && gameBoardChild[a] === gameBoardChild[c]) {
-                return gameBoardChild[a]
+    const checkWinner = () => {
+        for (const combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return board[a];
             }
         }
-        return null
+        return null;
+    };
+
+    const clearBoard = () => {
+        boardElements.forEach(boardElement => {
+            boardElement.textContent = '';
+            board.splice(0);
+        });
     }
-    return { value, gameBoardChild, winning }
-})()
 
-const boards = document.querySelectorAll('.board')
-const main = document.querySelector('main')
-const dialog = document.querySelector('.dialog')
-const startBtn = document.getElementById('start')
-const sideBar = document.querySelector('.sidebar')
-const resetBtn = document.getElementById('reset')
+    return { players, board, checkWinner, clearBoard };
+})();
 
-startBtn.addEventListener('click', () => {
-    main.classList.add('show')
-    dialog.classList.add('hide')
-    sideBar.classList.add('show')
-})
+// Player Factory Functions
+const Player = function (name, symbol) {
+    let score = 0;
+    const increaseScore = () => score++;
+    const getScore = () => score;
+    const resetScore = () => score = 0
 
-const players = function () {
-    let name = 0
-    let score = 0
-    const increaseScore = () => score++
-    const getScore = () => score
-    
-    const playerScore = () => {
-        
-    }
-    return { increaseScore, getScore, index }
-}
+    return { name, symbol, increaseScore, getScore, resetScore };
+};
 
-const player1 = players()
-const player2 = players()
-let player = 0
+// Game Module
+const game = (() => {
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+    let currentPlayer = player1;
 
-if (player === 0) {
-    player1.name = 'X'
-}
-player2.name = 'O'
-
-boards.forEach((board, index) => {
-    board.addEventListener('click', () => {
-        if (!board.textContent) {
-            const choice = gameBoard.value[player]
-            board.textContent = choice
-
-            gameBoard.gameBoardChild[index] = choice
+    const startGame = (boardElement, index) => {
+        if (!gameBoard.board[index]) {
+            const choice = currentPlayer.symbol;
+            boardElement.textContent = choice;
+            gameBoard.board[index] = choice;
         }
-        
-        const winner = gameBoard.winning()
+    }
+
+    const displayWinner = () => {
+        const winner = gameBoard.checkWinner();
         if (winner) {
-            alert(`Player ${winner} wins`)
-        } else {
-            player = (player + 1) % 2
-            
+            span.textContent = winner
+            popUp.showModal()
         }
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+
         if (winner === 'X') {
             player1.increaseScore()
-        }else {
+            player1Score.textContent = player1.getScore();
+        } else if (winner === 'O') {
             player2.increaseScore()
+            player2Score.textContent = player2.getScore();
         }
-    })
+
+    }
+
+    const resetGame = () => {
+        gameBoard.clearBoard()
+        player1.resetScore()
+        player2.resetScore()
+
+        player1Score.textContent = player1.getScore();
+        player2Score.textContent = player2.getScore();
+    }
+
+    return { startGame, displayWinner, resetGame }
+})()
+
+// Event Listeners
+startBtn.addEventListener('click', () => {
+    main.classList.add('show');
+    dialog.classList.add('hide');
+    sideBar.classList.add('show');
+});
+
+resetBtn.addEventListener('click', () => {
+    game.resetGame()
 })
+
+continueBtn.addEventListener('click', () => {
+    popUp.close()
+    gameBoard.clearBoard();
+})
+
+closeBtn.addEventListener('click', () => {
+    popUp.close()
+    game.resetGame()
+    main.classList.remove('show')
+    sideBar.classList.remove('show')
+    dialog.classList.remove('hide')
+})
+
+boardElements.forEach((boardElement, index) => {
+    boardElement.addEventListener('click', () => {
+
+        game.startGame(boardElement, index)
+        game.displayWinner()
+    });
+});
